bd5e805a1c0dbfd3fe91fde7d098d380235856ad
fwoma <136750885+fwoma@users.noreply.github.com>
2024-01-06 00:53:02 -0500

    Update push.sh

--- push/push.sh
+++ push/push.sh
@@ -4,12 +4,13 @@ echo "########################"
 echo "*** Preparing to push ***"
 echo "########################"
 
-REGISTRY="linuxfacilito.online:5043"
-IMAGE="app"
+##REGISTRY="linuxfacilito.online:5043"
+IMAGE="nginx"  # Change this to "nginx"
+TAG="latest"  # You can use a specific version or tag
 
 echo "*** Logging in ***"
 docker login -u woma702 -p $PASS $REGISTRY
 echo "*** Tagging image ***"
-docker tag $IMAGE:$BUILD_TAG $REGISTRY/$IMAGE:$BUILD_TAG
+docker tag $IMAGE:$TAG $REGISTRY/$IMAGE:$TAG
 echo "*** Pushing image ***"
-docker push $REGISTRY/$IMAGE:$BUILD_TAG
+docker push $REGISTRY/$IMAGE:$TAG
